0fa1dc8a0e1cd875bdcfacad497a1dd1362ef18a
ZackMccallum <ztmccallum@oakland.edu>
2021-12-06 00:22:44 -0500

    added test scripting logic

---
+++ web/src/js/buttonpress.js
@@ -0,0 +1,14 @@
+<script>
+const shell = require('shelljs')
+
+
+function launchdm() {
+  shell.exec('./launchdm.sh')
+}
+function launchcomp() {
+  shell.exec('./launchcomp.sh')
+}
+function launchcasual() {
+  shell.exec('./launchcasual.sh')
+}
+</script>
